a521a4b31bf24c592cfaba336c073e8972a17500
Casey Bateman <Casey.Bateman@hudl.com>
2014-05-30 13:50:11 -0500

    MED-1062 added support in Hudl.Ffmpeg to enable better streaming support in h264

---
+++ Hudl.Ffmpeg/Settings/MovFlags.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Hudl.Ffmpeg.BaseTypes;
+using Hudl.Ffmpeg.Common;
+using Hudl.Ffmpeg.Resources.BaseTypes;
+using Hudl.Ffmpeg.Settings.BaseTypes;
+
+namespace Hudl.Ffmpeg.Settings
+{
+    /// <summary>
+    /// MovFlags sets the LibAV Encoder options to enable features and options in the video
+    /// </summary>
+    [AppliesToResource(Type = typeof(IAudio))]
+    [AppliesToResource(Type = typeof(IVideo))]
+    [SettingsApplication(PreDeclaration = true, ResourceType = SettingsCollectionResourceType.Output)]
+    public class MovFlags : BaseSetting
+    {
+        private const string SettingType = "-movflags";
+        public const string EnableFastStart = "+faststart"; 
+
+        public MovFlags(string flags)
+            : base(SettingType)
+        {
+            Flags = flags;
+        }
+    
+        public string Flags { get; set; }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrWhiteSpace(Flags))
+            {
+                throw new InvalidOperationException("MovFlags cannot be null or empty.");
+            }
+
+            return string.Concat(Type, " ", Flags);
+        }
+    }
+}
