024853f4020a0e18d8a6430c694208a619cbd31c
Casey Bateman <Casey.Bateman@hudl.com>
2014-06-30 13:26:11 -0500

    NOTICK: added bitstream filters

---
+++ Hudl.Ffmpeg/Settings/BitStreamFilterAudio.cs
@@ -0,0 +1,21 @@
+﻿using Hudl.Ffmpeg.BaseTypes;
+using Hudl.Ffmpeg.Common;
+using Hudl.Ffmpeg.Resources.BaseTypes;
+using Hudl.Ffmpeg.Settings.BaseTypes;
+
+namespace Hudl.Ffmpeg.Settings
+{
+    [AppliesToResource(Type = typeof(IAudio))]
+    [SettingsApplication(PreDeclaration = true, ResourceType = SettingsCollectionResourceType.Output)]
+    public class BitStreamFilterAudio : BaseBitStreamFilter
+    {
+        private const string Suffix = ":a";
+
+        public const string ConvertAdtsToAsc = "aac_adtstoasc";
+
+        public BitStreamFilterAudio(string setting)
+            : base(Suffix, setting)
+        {
+        }
+    }
+}
