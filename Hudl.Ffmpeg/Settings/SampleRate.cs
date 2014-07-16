45b5e1f0e4d2a5e50da0a93f1f4b9fc850b21ed6
Casey Bateman <Casey.Bateman@hudl.com>
2014-07-16 10:59:12 -0500

    NOTICK: add sample rate for audio

---
+++ Hudl.Ffmpeg/Settings/SampleRate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Hudl.Ffmpeg.BaseTypes;
+using Hudl.Ffmpeg.Common;
+using Hudl.Ffmpeg.Resources.BaseTypes;
+using Hudl.Ffmpeg.Settings.BaseTypes;
+
+namespace Hudl.Ffmpeg.Settings
+{
+    [AppliesToResource(Type = typeof(IAudio))]
+    [SettingsApplication(PreDeclaration = true, ResourceType = SettingsCollectionResourceType.Output)]
+    public class SampleRate : BaseSetting
+    {
+        private const string SettingType = "-ar";
+
+        public SampleRate(double rate)
+            : base(SettingType)
+        {
+            if (rate <= 0)
+            {
+                throw new ArgumentException("Sample rate must be greater than zero.");
+            }
+
+            Rate = rate;
+        }
+
+        public double Rate { get; set; }
+
+        public override string ToString()
+        {
+            if (Rate <= 0)
+            {
+                throw new InvalidOperationException("Sample rate must be greater than zero.");
+            }
+
+            return string.Concat(Type, " ", Rate);
+        }
+    }
+}
