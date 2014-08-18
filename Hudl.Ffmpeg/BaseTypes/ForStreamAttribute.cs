7aeb2c976cf23b52f1df2e8daa45048dc3ca2351
Casey Bateman <Casey.Bateman@hudl.com>
2014-08-18 15:23:16 -0500

    changed the access identifiers for contains and for stream attributes

--- Hudl.Ffmpeg/BaseTypes/ForStreamAttribute.cs
+++ Hudl.Ffmpeg/BaseTypes/ForStreamAttribute.cs
@@ -6,7 +6,7 @@ namespace Hudl.FFmpeg.BaseTypes
     /// class level attribute that sets up a connection between a type with a resource
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple=true, Inherited=true)]
-    class ForStreamAttribute : Attribute
+    public class ForStreamAttribute : Attribute
     {
         public Type Type { get; set; }
     }
