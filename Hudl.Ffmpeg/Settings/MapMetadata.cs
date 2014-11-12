efec033701d92489f820d93462fdce19ae9ca9c6
Casey Bateman <Casey.Bateman@hudl.com>
2014-11-12 09:32:24 -0600

    add new map settings

---
+++ Hudl.Ffmpeg/Settings/MapMetadata.cs
@@ -0,0 +1,28 @@
+﻿using Hudl.Ffmpeg.BaseTypes;
+using Hudl.Ffmpeg.Common;
+using Hudl.Ffmpeg.Resources.BaseTypes;
+using Hudl.Ffmpeg.Settings.BaseTypes;
+
+namespace Hudl.Ffmpeg.Settings
+{
+    [AppliesToResource(Type = typeof(IVideo))]
+    [AppliesToResource(Type = typeof(IAudio))]
+    [SettingsApplication(PreDeclaration = true, ResourceType = SettingsCollectionResourceType.Output)]
+    public class MapMetadata : BaseSetting
+    {
+        private const string SettingType = "-map_metadata";
+
+        public MapMetadata(int index)
+            : base(SettingType)
+        {
+            Index = index;
+        }
+
+        public int Index { get; set; }
+
+        public override string ToString()
+        {
+            return string.Concat(Type, " ", Index);
+        }
+    }
+}
