a8950c6a80df5809d69afda36bf28983b126b587
Casey Bateman <Casey.Bateman@hudl.com>
2015-07-10 08:11:12 -0500

    add additional container support

---
+++ Hudl.Ffmpeg/Resources/Vob.cs
@@ -0,0 +1,23 @@
+﻿using Hudl.FFmpeg.Attributes;
+using Hudl.FFmpeg.Resources.BaseTypes;
+using Hudl.FFmpeg.Resources.Interfaces;
+
+namespace Hudl.FFmpeg.Resources
+{
+    [ContainsStream(Type = typeof(AudioStream))]
+    [ContainsStream(Type = typeof(VideoStream))]
+    public class Vob : BaseContainer
+    {
+        private const string FileFormat = ".vob";
+
+        public Vob()
+            : base(FileFormat)
+        {
+        }
+
+        protected override IContainer Clone()
+        {
+            return new Vob();
+        }
+    }
+}
