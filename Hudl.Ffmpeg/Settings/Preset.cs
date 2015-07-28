0b28d952f929d2eccce587a4685a613baaf8d66c
Paul Poulsen <paul.poulsen@hudl.com>
2015-07-28 13:16:34 -0500

    Added Tune and Preset Settings.

---
+++ Hudl.Ffmpeg/Settings/Preset.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Hudl.Ffmpeg.BaseTypes;
+using Hudl.Ffmpeg.Common;
+using Hudl.Ffmpeg.Resources.BaseTypes;
+using Hudl.Ffmpeg.Settings.BaseTypes;
+
+namespace Hudl.Ffmpeg.Settings
+{
+    [AppliesToResource(Type = typeof(IVideo))]
+    [SettingsApplication(ResourceType = SettingsCollectionResourceType.Output)]
+    public class Preset: BaseSetting
+    {
+        private const string SettingType = "-preset";
+
+        public Preset(string library)
+            : base(SettingType)
+        {
+            if (string.IsNullOrWhiteSpace(library))
+            {
+                throw new ArgumentNullException("library");
+            }
+
+            Library = library;
+        }
+        public Preset(PresetType library)
+            : this(Formats.Library(library))
+        {
+        }
+
+        public string Library { get; set; }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrWhiteSpace(Library))
+            {
+                throw new InvalidOperationException("Library cannot be empty for this setting.");
+            }
+
+            return string.Concat(Type, " ", Library);
+        }
+    }
+}
---
+++ Hudl.Ffmpeg/Settings/Tune.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Hudl.Ffmpeg.BaseTypes;
+using Hudl.Ffmpeg.Common;
+using Hudl.Ffmpeg.Resources.BaseTypes;
+using Hudl.Ffmpeg.Settings.BaseTypes;
+
+namespace Hudl.Ffmpeg.Settings
+{
+    [AppliesToResource(Type = typeof(IVideo))]
+    [SettingsApplication(ResourceType = SettingsCollectionResourceType.Output)]
+    public class Tune: BaseSetting
+    {
+        private const string SettingType = "-tune";
+
+        public Tune(string library)
+            : base(SettingType)
+        {
+            if (string.IsNullOrWhiteSpace(library))
+            {
+                throw new ArgumentNullException("library");
+            }
+
+            Library = library;
+        }
+        public Tune(TuneType library)
+            : this(Formats.Library(library))
+        {
+        }
+
+        public string Library { get; set; }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrWhiteSpace(Library))
+            {
+                throw new InvalidOperationException("Library cannot be empty for this setting.");
+            }
+
+            return string.Concat(Type, " ", Library);
+        }
+    }
+}
