fdcc29e4f842164401d6b51b9cc3198ec6fc8424
Joel Hensley <joel.hensley@hudl.com>
2016-05-26 14:18:15 -0500

    Bump assembly version to 3.5.0

--- Hudl.Ffmpeg/Properties/AssemblyInfo.cs
+++ Hudl.Ffmpeg/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyInformationalVersion("3.5.0-beta1")]
-[assembly: AssemblyFileVersion("3.5.0-beta1")]
+[assembly: AssemblyInformationalVersion("3.5.0")]
+[assembly: AssemblyFileVersion("3.5.0")]
 [assembly: AssemblyVersion("3.0.0.0")]
