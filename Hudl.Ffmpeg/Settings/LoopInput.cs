0bee43fdb3ab1b6ed114f08df5f7ff30d8d57770
Joel Hensley <joel.hensley@hudl.com>
2016-08-23 21:04:33 -0500

    Added LoopInput input setting

---
+++ Hudl.Ffmpeg/Settings/LoopInput.cs
@@ -0,0 +1,22 @@
+﻿using Hudl.FFmpeg.Attributes;
+using Hudl.FFmpeg.Enums;
+using Hudl.FFmpeg.Resources.BaseTypes;
+using Hudl.FFmpeg.Settings.Attributes;
+using Hudl.FFmpeg.Settings.Interfaces;
+
+namespace Hudl.FFmpeg.Settings
+{
+    /// <summary>
+    /// Used to loop input image streams
+    /// </summary>
+    [ForStream(Type = typeof (VideoStream))]
+    [Setting(Name = "loop", IsPreDeclaration = true, ResourceType = SettingsCollectionResourceType.Input)]
+    public class LoopInput : ISetting
+    {
+        [SettingParameter]
+        public string Flags
+        {
+            get { return "1"; }
+        }
+    }
+}
