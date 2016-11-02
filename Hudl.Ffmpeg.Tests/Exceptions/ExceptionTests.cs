44dbc713964d5202289c834e248912e0b3f1429b
Casey Bateman <Casey.Bateman@hudl.com>
2016-11-02 13:26:19 -0500

    add some more tests to build up coverage where needed

---
+++ Hudl.Ffmpeg.Tests/Exceptions/ExceptionTests.cs
@@ -0,0 +1,37 @@
+﻿using Hudl.FFmpeg.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hudl.FFmpeg.Tests.Exceptions
+{
+    public class ExceptionTests
+    {
+        [Fact]
+        public void ExceptionsRaised()
+        {
+            Assert.Throws<FFmpegProcessingException>(() => {
+                throw new FFmpegProcessingException(1, "foo"); 
+            });
+
+            Assert.Throws<FFmpegRenderingException>(() => {
+                throw new FFmpegRenderingException(null);
+            });
+
+            Assert.Throws<ForStreamInvalidException>(() => {
+                throw new ForStreamInvalidException(typeof(string) , typeof(string));
+            });
+
+            Assert.Throws<StreamNotFoundException>(() => {
+                throw new StreamNotFoundException(typeof(string));
+            });
+
+            Assert.Throws<StreamNotFoundException>(() => {
+                throw new StreamNotFoundException();
+            });
+        }
+    }
+}
