926bdd1e162543747095751eb2c44c20e02b27d7
Casey Bateman <Casey.Bateman@hudl.com>
2017-01-12 10:46:51 -0600

    change assembly version at major release

--- Hudl.FFmpeg.Core/Properties/AssemblyInfo.cs
+++ Hudl.FFmpeg.Core/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("5.0.0")]
 [assembly: AssemblyFileVersion("5.0.0")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
--- Hudl.FFmpeg/Properties/AssemblyInfo.cs
+++ Hudl.FFmpeg/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyInformationalVersion("5.0.0")]
 [assembly: AssemblyFileVersion("5.0.0")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
