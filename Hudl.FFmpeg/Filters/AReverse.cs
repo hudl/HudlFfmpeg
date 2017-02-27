a69f99cdc817054fd1a979f939e81573e9e4b164
Ben <ben.rubinger@hudl.com>
2017-02-27 10:05:04 -0500

    Add support for Reverse and AReverse filters

---
+++ Hudl.FFmpeg/Filters/AReverse.cs
@@ -0,0 +1,14 @@
+﻿using Hudl.FFmpeg.Attributes;
+using Hudl.FFmpeg.Filters.Attributes;
+using Hudl.FFmpeg.Filters.Interfaces;
+using Hudl.FFmpeg.Resources.BaseTypes;
+
+namespace Hudl.FFmpeg.Filters
+{
+    [ForStream(Type = typeof(AudioStream))]
+    [Filter(Name ="areverse", MinInputs = 1, MaxInputs = 1)]
+    public class AReverse : IFilter
+    {
+        //This filter accepts no parameters
+    }
+}
---
+++ Hudl.FFmpeg/Filters/Reverse.cs
@@ -0,0 +1,14 @@
+﻿using Hudl.FFmpeg.Attributes;
+using Hudl.FFmpeg.Filters.Attributes;
+using Hudl.FFmpeg.Filters.Interfaces;
+using Hudl.FFmpeg.Resources.BaseTypes;
+
+namespace Hudl.FFmpeg.Filters
+{
+    [ForStream(Type = typeof(VideoStream))]
+    [Filter(Name ="reverse", MinInputs = 1, MaxInputs = 1)]
+    public class Reverse : IFilter
+    {
+        //This filter accepts no parameters
+    }
+}
