b4eae1ffbb941616d232683f40ff4a1e6a12f552
Ben <ben.rubinger@hudl.com>
2017-02-27 10:06:53 -0500

    Add filter comment

--- Hudl.FFmpeg/Filters/AReverse.cs
+++ Hudl.FFmpeg/Filters/AReverse.cs
@@ -5,6 +5,10 @@ using Hudl.FFmpeg.Resources.BaseTypes;
 
 namespace Hudl.FFmpeg.Filters
 {
+    /// <summary>
+    /// Reverses an audio clip. Note that it will buffer the entire clip,
+    /// so it's recommented to use trimming.
+    /// </summary>
     [ForStream(Type = typeof(AudioStream))]
     [Filter(Name ="areverse", MinInputs = 1, MaxInputs = 1)]
     public class AReverse : IFilter
--- Hudl.FFmpeg/Filters/Reverse.cs
+++ Hudl.FFmpeg/Filters/Reverse.cs
@@ -5,6 +5,10 @@ using Hudl.FFmpeg.Resources.BaseTypes;
 
 namespace Hudl.FFmpeg.Filters
 {
+    /// <summary>
+    /// Reverses a video clip. Note that it will buffer the entire clip,
+    /// so it's recommented to use trimming.
+    /// </summary>
     [ForStream(Type = typeof(VideoStream))]
     [Filter(Name ="reverse", MinInputs = 1, MaxInputs = 1)]
     public class Reverse : IFilter
