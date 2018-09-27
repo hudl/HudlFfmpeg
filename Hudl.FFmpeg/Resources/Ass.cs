1af7f2013d6ec9d6575056fc0b22ea5488aa175e
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2018-09-27 23:11:12 +0200

    Included srt and ass file format

---
+++ Hudl.FFmpeg/Resources/Ass.cs
@@ -0,0 +1,22 @@
+﻿using Hudl.FFmpeg.Attributes;
+using Hudl.FFmpeg.Resources.BaseTypes;
+using Hudl.FFmpeg.Resources.Interfaces;
+
+namespace Hudl.FFmpeg.Resources
+{
+    [ContainsStream(Type = typeof(SubtitleStream))]
+    public class Ass : BaseContainer
+    {
+        private const string FileFormat = ".ass";
+
+        public Ass()
+            : base(FileFormat)
+        {
+        }
+
+        protected override IContainer Clone()
+        {
+            return new Ass();
+        }
+    }
+}
---
+++ Hudl.FFmpeg/Resources/Srt.cs
@@ -0,0 +1,22 @@
+﻿using Hudl.FFmpeg.Attributes;
+using Hudl.FFmpeg.Resources.BaseTypes;
+using Hudl.FFmpeg.Resources.Interfaces;
+
+namespace Hudl.FFmpeg.Resources
+{
+    [ContainsStream(Type = typeof(SubtitleStream))]
+    public class Srt : BaseContainer
+    {
+        private const string FileFormat = ".srt";
+
+        public Srt()
+            : base(FileFormat)
+        {
+        }
+
+        protected override IContainer Clone()
+        {
+            return new Srt();
+        }
+    }
+}
