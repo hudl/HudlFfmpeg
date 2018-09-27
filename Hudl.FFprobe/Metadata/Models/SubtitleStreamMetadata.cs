c7d0bd5c4e7feca62d52c95a3708bc5d73243bc1
Alexei Agüero Alba <alexeiagueroalba@gmail.com>
2018-09-27 20:35:56 +0200

    Created Subtitle Frame and Stream metadata

---
+++ Hudl.FFprobe/Metadata/Models/SubtitleStreamMetadata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Hudl.FFprobe.Metadata.Models
+{
+    [JsonObject]
+    public class SubtitleStreamMetadata : BaseStreamMetadata
+    {
+    }
+}
