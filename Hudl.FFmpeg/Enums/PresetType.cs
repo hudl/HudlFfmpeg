b3b613b42510f9310d7dde733001f3c861d1c606
Simon Bühler <simon@00010111.de>
2019-01-10 12:01:30 +0100

    add preset support

---
+++ Hudl.FFmpeg/Enums/PresetType.cs
@@ -0,0 +1,25 @@
+﻿namespace Hudl.FFmpeg.Enums
+{
+
+    /// <summary>
+    /// enumeration containing the known preset types
+    /// The general guideline is to use the slowest preset that you have patience for.
+    /// Current presets in descending order of speed are: ultrafast,superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo.
+    /// The default preset is medium. Ignore placebo as it is not useful (see https://trac.ffmpeg.org/wiki/Encode/H.264#FAQ).
+    /// You can see a list of current presets with -preset help (see example below), and what settings they apply with x264 --fullhelp.
+    /// https://trac.ffmpeg.org/wiki/Encode/H.264#a2.Chooseapreset
+    /// </summary>
+    public enum PresetType
+    {
+        ultrafast,
+        superfast,
+        veryfast,
+        faster,
+        fast,
+        medium,
+        slow,
+        slower,
+        veryslow,
+        placebo
+    } 
+}
